12370ea926afaeba0d8df87a512c35b3a657d67a
stephen yi <stephenyi@stephens-MacBook-Pro.local>
2020-09-17 04:12:46 -0400

    part1 working

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def snakes_index(request):
+    return render(request, 'snakes/index.html', { 'snakes': snakes})
+
+class Snake:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, species, description, age):
+    self.name = name
+    self.species = species
+    self.description = description
+    self.age = age
+
+snakes = [
+  Snake('Bean', 'Black Mamba', 'foul little demon', 3),
+  Snake('Kaa', 'Python', 'diluted tortoise shell', 0),
+  Snake('Voldemort', 'Viper', '3 legged cat', 4)
+]
